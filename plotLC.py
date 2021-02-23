0b2b0bbf1c4ea8a3cab9caa9a14439e5203ec49b
dario <dariogonzalezpicos@gmail.com>
2021-02-23 14:09:12 +0000

    Fixed reading issues with bad characters and large error bars

--- plotLC.py
+++ plotLC.py
@@ -2,14 +2,15 @@
 Created on Sun Feb 14 23:24:46 2021
 @author: dario
 
-Title: Plotting lightcurves from 2 ASAS-SN sources 
+Title: Plotting lightcurves from rnadom LCs from the Full Catalog of
+ Variable ASAS-SN sources 
 """
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import glob
-from astropy.io import ascii
+from astropy.io import ascii 
 
 basepath = '/home/dario/AstronomyLeiden/FRP/ASAS' # path to images
 os.chdir(basepath)
@@ -18,6 +19,8 @@ file_list = glob.glob(basepath + '/*.dat')
 file_list[4]
 N = len(file_list)
 print('Number of files in the directory ... ', N)
+
+
 
 #%%
 print(pd.read_csv('AP18326566.csv').columns)
@@ -28,34 +31,51 @@ print(pd.read_csv('AP18326566.csv').columns)
 
 def loadmultdata(rand_files):
     # converters = {'col3': [ascii.convert_numpy(np.float)]}
-    hjd, mag, errmag, names, temp_list = [[] for i in range(5)]
+    hjd, mag, errmag, names = [[] for i in range(4)]
     for i in rand_files:
         data = ascii.read(file_list[i])
         hjd.append(data['col1'])
-        
-        # for string in data['col3']:
-        #     temp_list.append(float(str(string).replace('>', '')))
-                
-        # mag.append(temp_list)
-        mag.append(data['col3'])
         errmag.append(data['col4'])
+        
+        temp_list = []
+        for item in data['col3']:
+            a  = str(type(item))
+            if a == "<class 'numpy.str_'>":
+                temp_list.append(float(item.split('>')[-1]))
+            else:
+                temp_list.append(item)
+        
+        mag.append(temp_list)
+        
+        
         
         name = file_list[i].split('/')[-1][2:-4]
         names.append(name)
     return hjd, mag, errmag, names
 
-# Probably better to put the csv files on a folder and get
-# all the files from the folder 
+
 
 # Call function
-# files = ['AP18326566.csv', 'AP18326567.csv', 'AP28742359.csv']
-
 # choose XX random files to analyze
-rand_files = np.random.randint(0, N, 3)
+rand_files = np.random.randint(0, N, 4)
 hjd, mag, errmag, names = loadmultdata(rand_files)
 
-hjd
-mag
+errmag 
+# Fix "infinite values" for the "errmag" data by
+# replacing them with the median
+
+for j in range(len(errmag)):
+    med = np.median(errmag[j])
+    if med > 90:
+        print(f'Error bars not valid for plot {names[j]}')
+        
+    for count, k in enumerate(errmag[j]):
+        if k > 90: # the wrong values have an assigned "99.99"
+            if med < 90:
+                errmag[j][count] = np.median(errmag[j])
+            else:
+                errmag[j][count] = 0.0 # no value
+            
 # Plot LCs
 fig, ax = plt.subplots(len(hjd), 1, figsize=(15,16))
    
@@ -72,7 +92,4 @@ for j in range(len(hjd)):
 # plt.savefig('LC1.png', dpi=100, bbox_inches='tight')
 plt.savefig('/home/dario/AstronomyLeiden/FRP/MRP-ASAS-Rings/LC3.png', dpi=100, bbox_inches='tight')
 plt.show()
-#%% 
-df = pd.read_csv(files[0])
-df.columns
-files[0][:-4]
+
